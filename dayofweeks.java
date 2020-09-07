eaddff51572e26c52138f9606a23a83b93fc070f
Md Noman Khan <54404474+noman2002@users.noreply.github.com>
2020-09-08 00:33:30 +0530

    Add files via upload

---
+++ dayofweeks.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class dayofweeks {
+    public static void main(final String s[]) {
+        final Scanner x = new Scanner(System.in);
+        final int day = x.nextInt();
+        x.close();
+        switch(day)
+        {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+        }
+    }
+}
