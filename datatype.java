350da49d6780501a1c13237d86f7b44a6e9b2840
Md Noman Khan <54404474+noman2002@users.noreply.github.com>
2020-09-08 00:33:08 +0530

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,21 @@
+public class datatype {
+
+   static boolean Boolean;
+   static double Double;
+   static float Float;
+   static int Int;
+   static long Long;
+   static String string;
+    
+    public static void main(String[] args) {
+       
+       
+       System.out.println("Default values.....");
+       System.out.println("Boolean = " + Boolean);
+       System.out.println("Double = " + Double);
+       System.out.println("Float = " + Float);
+       System.out.println("Int = " + Int);
+       System.out.println("Long = " + Long);
+       System.out.println("string = " + string);
+    }
+ }
