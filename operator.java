1614c1118a722df4857eb024b02b5f26f313be67
Md Noman Khan <54404474+noman2002@users.noreply.github.com>
2020-09-08 00:34:35 +0530

    Add files via upload

---
+++ operator.java
@@ -0,0 +1,19 @@
+public class operator {
+     public static void main(String[] args) {
+        int a=10;
+        a+=3;
+        System.out.println(a);
+        a-=4;
+        System.out.println(a);
+        a*=2;
+        System.out.println(a);
+short x=10;
+short y=10;
+x+=y;
+System.out.println(x);
+x=(short)(x+y);
+System.out.println(x);
+
+
+    }
+}
